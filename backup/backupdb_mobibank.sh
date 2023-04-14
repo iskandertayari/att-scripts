1dad3372fc51c81fcded27138d882ff675ea28f7
iskandertayari <118175521+iskandertayari@users.noreply.github.com>
2023-04-14 10:57:35 +0100

    Create backupdb_mobibank.sh

---
+++ backup/backupdb_mobibank.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -x
+#
+# backup database 
+#
+
+HOSTADDRESS="172.30.2.99"
+DATABASE="mobibank"
+USER="mobibank"
+PASSWORD=""
+ERRORFILE="/var/tmp/psqlerror.err"
+DAY="`date +'%Y-%m-%d'`"
+MONTH="`date +'%Y-%m'`"
+SHAREFOLDER="/mnt/backup"
+BACKUPDIR="$SHAREFOLDER/databases"
+PERIOD="daily"
+
+# Welcome
+echo "Backup script started $0" 
+echo "Today: `date +"%Y-%m-%d %M:%S"`"
+
+# Test database connection
+TEMP="`psql -h $HOSTADDRESS -U $USER -l 2>$ERRORFILE`"
+
+if [[ -n `cat $ERRORFILE |grep 'Connection refused'` ]] ; then
+        echo "Error: Failed to connect to $HOSTADDRESS"
+        exit 1
+fi
+
+if [[ -n `cat $ERRORFILE |grep 'FATAL:  role'` ]]; then
+        echo "Error: User '$USER' does not exist"
+        exit 2
+fi
+
+unset TEMP
+
+TEMP="`psql -h $HOSTADDRESS -U $USER -l | awk '{print $1}' |grep -w $DATABASE`"
+
+if [[ -z $TEMP ]]; then
+        echo "Error: Database '$DATABASE' does not exist"
+        exit 3
+fi
+
+# Create mount directory
+if [[ ! -d $SHAREFOLDER ]]; then
+        mkdir -p $SHAREFOLDE
+fi
+
+if grep -qs $SHAREFOLDER /proc/mounts; then
+    echo "Shared folder already mounted."
+else
+    echo "Shared folder not mounted, mounting it."
+    mount.cifs //172.28.80.30/base $SHAREFOLDER -o username=backup30,password=backup30
+    if [[ $? != 0 ]]; then
+        echo "Error: Failed to mount shared disk, backup process stopped!"
+        exit 4
+    fi
+    echo "Shared folder is mounted successfully."
+fi
+
+# Create databases folder
+if [[ ! -d $BACKUPDIR ]]; then
+        mkdir -p $BACKUPDIR
+fi
+ 
+# Backup database
+echo -n "Starting Backup '$DATABASE' ... " 
+if [[ $PERIOD = "daily" ]];then
+pg_dump -h $HOSTADDRESS -U $USER $DATABASE > $BACKUPDIR/$DATABASE-$DAY.sql 2>$ERRORFILE 
+gzip $BACKUPDIR/$DATABASE-$DAY.sql 2>$ERRORFILE
+elif [[ $PERIOD = "monthly" ]]; then
+pg_dump -h $HOSTADDRESS -U $USER $DATABASE > $BACKUPDIR/$DATABASE-$MONTH.sql 2>$ERRORFILE
+gzip $BACKUPDIR/$DATABASE-$MONTH.sql 2>$ERRORFILE
+fi
+
+if [[ $? != 0 ]]; then
+        echo "ERROR"
+else
+        echo "OK"
+fi
+# Unmount shared folder
+umount /mnt/backup 2>$ERRORFILE
+if [[ $? != 0 ]]; then
+        echo "Error: Failed to unmount shared folder."
+        exit 
+fi
+echo "Shared folder is unmounted successfully."
+
+# Clean files and temp folders
+rm -f $ERRORFILE
